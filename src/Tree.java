10ea7c79293f76e02823d9a73e0e8763fecee28d
Max Aird <blackberry3224@gmail.com>
2022-10-27 08:59:26 -0600

    added javadoc

--- src/Tree.java
+++ src/Tree.java
@@ -2,6 +2,10 @@
 
 public class Tree
 {
+    /**
+     * string with tree and escape sequences
+     * @return returned tree string
+     */
     public static String draw()
     {
         String treeBuilder = "       /\\ \n      /  \\ \n     /    \\ \n    /      \\  \n   /        \\ \n  ------------ \n    ''   ''  \n    ''   ''  \n    ''   ''  \n";
@@ -9,6 +13,9 @@ public class Tree
         return treeBuilder;
     }
 
+    /**
+     * start method
+     */
     public void start()
     {
         System.out.println(draw());
